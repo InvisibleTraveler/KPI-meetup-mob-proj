c39488ef16e1c4809e02ff39f2b7f50824f765a0
Vlad Lytvynenko <vlad.lytvynenko@djangostars.com>
2019-12-18 22:42:36 +0200

    added admin models

--- meetings/admin.py
+++ meetings/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Meeting
+from .models import Meeting, Tag
 
 
 @admin.register(Meeting)
@@ -7,3 +7,9 @@ class MeetingAdmin(admin.ModelAdmin):
     list_display = ("title", "created_at", "start_at", "date_string", "is_main")
     list_filter = ("title", "created_at", "is_main")
     search_fields = ("title",)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
