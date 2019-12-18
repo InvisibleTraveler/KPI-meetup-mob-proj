1447a2706fdef5ff4bd62219dceb4d55cad63cea
Vlad Lytvynenko <vlad.lytvynenko@djangostars.com>
2019-12-18 19:23:14 +0200

    token auth

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -64,7 +64,7 @@ class CreateRateSerializer(serializers.ModelSerializer):
         )
 
     def validate(self, attrs):
-        if attrs.get() == attrs.get():
+        if attrs.get("visitor") == attrs.get("lector"):
             raise ValidationError({"lector": "Can't set rate for yourself"})
         return attrs
 
